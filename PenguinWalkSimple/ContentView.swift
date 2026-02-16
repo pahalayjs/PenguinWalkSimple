3bc667c025045ac7c4d0a63649ecfd46031c95ad
pahalayjs <pahalayjs@gmail.com>
2026-02-16 13:06:37 +0700

    Initial Commit

---
+++ PenguinWalkSimple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PenguinWalkSimple
+//
+//  Created by Pahala Sihombing on 16/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PenguinWalkSimple/PenguinWalkSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenguinWalkSimpleApp.swift
+//  PenguinWalkSimple
+//
+//  Created by Pahala Sihombing on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PenguinWalkSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
